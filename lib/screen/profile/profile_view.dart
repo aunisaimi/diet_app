865e5f0af7bda3ac0f9c1b6c9a4c16f3367b3039
aunisaimi <129945684+aunisaimi@users.noreply.github.com>
2024-06-10 23:37:17 +0800

    history

--- lib/screen/profile/profile_view.dart
+++ lib/screen/profile/profile_view.dart
@@ -32,10 +32,13 @@ class _ProfileViewState extends State<ProfileView> {
   TextEditingController _goalController = TextEditingController();
   String profilePicture = '';
 
+  List<Map<String, dynamic>> historyList = []; // State variable to store history data
+
   @override
   void initState() {
     super.initState();
     fetchUserData();
+    fetchUserHistory();
   }
 
   Future<void> fetchUserData() async {
@@ -63,7 +66,6 @@ class _ProfileViewState extends State<ProfileView> {
         });
         print("This is the current user email: ${userDoc['email']}");
         print("This is the current user name: ${userDoc['fname']}");
-
       } else {
         print("Data not exist");
       }
@@ -72,10 +74,211 @@ class _ProfileViewState extends State<ProfileView> {
     }
   }
 
+  Future<void> fetchUserHistory() async {
+    try {
+      final userId = FirebaseAuth.instance.currentUser!.uid;
+
+      // Fetch the user's history from Firestore
+      final historySnapshot = await FirebaseFirestore.instance
+          .collection('history')
+          .where('userId', isEqualTo: userId)
+          .orderBy('timestamp', descending: true)
+          .get();
+
+      List<Map<String, dynamic>> fetchedHistory = historySnapshot.docs.map((doc) {
+        final data = doc.data() as Map<String, dynamic>;
+
+        // Check if the required fields exist and handle missing fields gracefully
+        final exercise = data.containsKey('exercise') ? data['exercise'] : {};
+        final name = exercise.containsKey('name') ? exercise['name'] : 'Unknown Exercise';
+        final title = exercise.containsKey('title') ? exercise['title'] : 'No Title';
+        final status = data.containsKey('status') ? data['status'] : 'Unknown Status';
+        final userDetails = data.containsKey('userDetails') ? data['userDetails'] : {};
+        final bmi = userDetails.containsKey('bmi') ? userDetails['bmi'] : 'Unknown BMI';
+
+        print("Exercise: $name, Title: $title, Status: $status, BMI: $bmi"); // Debugging information
+
+        return {
+          'name': name,
+          'title': title,
+          'status': status,
+          'bmi': bmi,
+        };
+      }).toList();
+
+      setState(() {
+        historyList = fetchedHistory;
+      });
+
+    } catch (e) {
+      print("Error fetching history: $e");
+    }
+  }
+
+
+
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: TColor.white,
+  //       centerTitle: true,
+  //       elevation: 0,
+  //       leadingWidth: 0,
+  //       title: const Text(
+  //         "Profile",
+  //         style: TextStyle(
+  //           color: Colors.black,
+  //           fontSize: 18,
+  //           fontWeight: FontWeight.w700,
+  //         ),
+  //       ),
+  //       actions: [
+  //         InkWell(
+  //           onTap: () {
+  //             Navigator.push(
+  //                 context,
+  //                 MaterialPageRoute(
+  //                     builder: (context) => const ThemePage())
+  //             );
+  //           },
+  //           child: Container(
+  //             margin: const EdgeInsets.all(8),
+  //             height: 40,
+  //             width: 40,
+  //             alignment: Alignment.center,
+  //             decoration: BoxDecoration(
+  //               color: TColor.lightGray,
+  //               borderRadius: BorderRadius.circular(10),
+  //             ),
+  //             child: const Icon(Icons.more_horiz_rounded),
+  //           ),
+  //         )
+  //       ],
+  //     ),
+  //     backgroundColor: TColor.white,
+  //     body: SingleChildScrollView(
+  //       child: Container(
+  //         padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.stretch,
+  //           children: [
+  //             Row(
+  //               children: [
+  //                 ClipRRect(
+  //                   borderRadius: BorderRadius.circular(30),
+  //                   child: profilePicture.isNotEmpty
+  //                       ? Image.network(
+  //                       profilePicture,
+  //                       width: 50,
+  //                       height: 50,
+  //                       fit: BoxFit.cover)
+  //                       : Image.asset(
+  //                     "assets/img/workingcats.jpg",
+  //                     width: 50,
+  //                     height: 50,
+  //                     fit: BoxFit.cover,
+  //                   ),
+  //                 ),
+  //                 const SizedBox(width: 10),
+  //                 Expanded(
+  //                   child: Column(
+  //                     crossAxisAlignment: CrossAxisAlignment.start,
+  //                     children: [
+  //                       Text(
+  //                         "${_firstnameController.text} ${_lastnameController.text}",
+  //                         style: TextStyle(
+  //                           color: TColor.black,
+  //                           fontSize: 14,
+  //                           fontWeight: FontWeight.w600,
+  //                         ),
+  //                       ),
+  //                       Text(
+  //                         "${_goalController.text}", // Update this
+  //                         style: TextStyle(
+  //                           color: TColor.gray,
+  //                           fontSize: 12,
+  //                         ),
+  //                       )
+  //                     ],
+  //                   ),
+  //                 ),
+  //                 SizedBox(
+  //                   width: 70,
+  //                   height: 70,
+  //                   child: RoundButton(
+  //                     title: "Edit",
+  //                     type: RoundButtonType.bgSGradient,
+  //                     fontSize: 12,
+  //                     fontWeight: FontWeight.w400,
+  //                     onPressed: () {
+  //                       Navigator.push(
+  //                           context,
+  //                           MaterialPageRoute(
+  //                               builder: (context) => const EditProfile())
+  //                       );
+  //                     },
+  //                   ),
+  //                 ),
+  //               ],
+  //             ),
+  //             const SizedBox(height: 15),
+  //             Row(
+  //               children: [
+  //                 Expanded(
+  //                   child: TitleSubtitleCell(
+  //                     title: txtHeight.text,
+  //                     subtitle: "Height",
+  //                   ),
+  //                 ),
+  //                 const SizedBox(width: 15),
+  //                 Expanded(
+  //                   child: TitleSubtitleCell(
+  //                     title: txtWeight.text,
+  //                     subtitle: "Weight",
+  //                   ),
+  //                 ),
+  //                 const SizedBox(width: 15),
+  //                 Expanded(
+  //                   child: TitleSubtitleCell(
+  //                     title: _genderController.text,
+  //                     subtitle: "Gender",
+  //                   ),
+  //                 ),
+  //               ],
+  //             ),
+  //             const SizedBox(height: 20),
+  //             Text(
+  //               "Exercise History",
+  //               style: TextStyle(
+  //                 color: TColor.black,
+  //                 fontSize: 18,
+  //                 fontWeight: FontWeight.w700,
+  //               ),
+  //             ),
+  //             const SizedBox(height: 10),
+  //             ListView.builder(
+  //               physics: const NeverScrollableScrollPhysics(),
+  //               shrinkWrap: true,
+  //               itemCount: historyList.length,
+  //               itemBuilder: (context, index) {
+  //                 var historyItem = historyList[index];
+  //                 return ListTile(
+  //                   title: Text(historyItem['name']),
+  //                   subtitle: Text(
+  //                       'Status: ${historyItem['status']}\nBMI: ${historyItem['bmi']}'),
+  //                 );
+  //               },
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
-    //var media = MediaQuery.of(context).size;
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: TColor.white,
@@ -120,17 +323,18 @@ class _ProfileViewState extends State<ProfileView> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              // User profile information
               Row(
                 children: [
                   ClipRRect(
                     borderRadius: BorderRadius.circular(30),
                     child: profilePicture.isNotEmpty
                         ? Image.network(
-                      profilePicture,
-                      width: 50,
-                      height: 50,
-                      fit: BoxFit.cover)
-                        :Image.asset(
+                        profilePicture,
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover)
+                        : Image.asset(
                       "assets/img/workingcats.jpg",
                       width: 50,
                       height: 50,
@@ -205,6 +409,70 @@ class _ProfileViewState extends State<ProfileView> {
                   ),
                 ],
               ),
+              // User history information
+              const SizedBox(height: 20),
+              Text(
+                "Exercise History",
+                style: TextStyle(
+                  color: TColor.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(height: 15),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: historyList.length,
+                itemBuilder: (context, index) {
+                  var historyItem = historyList[index];
+                  return Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: TColor.lightGray,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          historyItem['name'],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: TColor.black,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          'Title: ${historyItem['title']}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: TColor.gray,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          'Status: ${historyItem['status']}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: TColor.gray,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          'BMI: ${historyItem['bmi']}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: TColor.gray,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
               // Other UI elements
             ],
           ),
@@ -212,4 +480,5 @@ class _ProfileViewState extends State<ProfileView> {
       ),
     );
   }
+
 }
