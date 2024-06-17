851516a2497151450e0e7fdc1b8a196c3b9427fe
aunisaimi <129945684+aunisaimi@users.noreply.github.com>
2024-06-18 00:08:59 +0800

    fixed the spelling at firebase

--- lib/screen/meal_planner/dietandfitness/MealPlanner.dart
+++ lib/screen/meal_planner/dietandfitness/MealPlanner.dart
@@ -1,76 +1,76 @@
-import 'package:cloud_firestore/cloud_firestore.dart';  // Importing Firestore package
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:diet_app/common/color_extension.dart';
-import 'package:diet_app/model/meal.dart';  // Importing custom meal model
+import 'package:diet_app/model/meal.dart';
 import 'package:diet_app/screen/meal_planner/dietandfitness/MealDetailScreen.dart';
 import 'package:diet_app/screen/meal_planner/dietandfitness/diet.dart';
 import 'package:flutter/material.dart';
 
 class MealPlanner extends StatefulWidget {
-  const MealPlanner({Key? key, required  dietType}) : super(key: key);
+  const MealPlanner({Key? key, required dietType}) : super(key: key);
 
   @override
   State<MealPlanner> createState() => _MealPlannerState();
 }
 
 class _MealPlannerState extends State<MealPlanner> {
-  List<Map<String, String>> dietTypes = [];  // List to hold diet types fetched from Firestore
-  String selectedDietId = '';  // Variable to hold the selected diet ID
-  String selectedDietName = '';  // Variable to hold the selected diet name
-  List<Meal> meals = [];  // List to hold meals fetched from Firestore
-  String selectedCategory = 'breakfast';  // Variable to hold the selected category, default is 'breakfast'
+  List<Map<String, String>> dietTypes = [];
+  String selectedDietId = '';
+  String selectedDietName = '';
+  List<Meal> meals = [];
+  String selectedCategory = 'breakfast';
 
   @override
   void initState() {
     super.initState();
-    _fetchDietTypes();  // Fetch diet types when the widget initializes
+    _fetchDietTypes();
   }
 
-  // Function to fetch diet types from Firestore
   Future<void> _fetchDietTypes() async {
     try {
       QuerySnapshot snapshot = await FirebaseFirestore.instance
           .collection('diets')
-          .get();  // Fetch all documents from the 'diets' collection
+          .get();
       setState(() {
-        dietTypes = snapshot.docs.map((doc) => {  // Map each document to a map with 'dietId' and 'name'
+        dietTypes = snapshot.docs.map((doc) => {
           'dietId': doc.id,
-          'name': doc['dietType'] as String
+          'name': doc['dietType'] as String,
         }).toList();
 
-        if (dietTypes.isNotEmpty) {  // If there are diet types fetched
-          selectedDietId = dietTypes.first['dietId']!;  // Set the first diet ID as selected
-          selectedDietName = dietTypes.first['name']!;  // Set the first diet name as selected
-          _fetchMeals(selectedDietId, selectedCategory);  // Fetch meals for the selected diet and category
+        if (dietTypes.isNotEmpty) {
+          selectedDietId = dietTypes.first['dietId']!;
+          selectedDietName = dietTypes.first['name']!;
+          _fetchMeals(selectedDietId, selectedCategory);
         }
       });
 
-      print('Diet types fetched: $dietTypes');  // Print fetched diet types
-
-    } catch (e,stackTrace) {
+      print('Diet types fetched: $dietTypes');
+    } catch (e, stackTrace) {
       print('Error fetching diet types: $e');
       print(stackTrace);
     }
   }
 
-  // Function to fetch meals from Firestore based on diet ID and category
   Future<void> _fetchMeals(String dietId, String category) async {
     try {
       print('Fetching meals for dietId: $dietId, category: $category');
 
       QuerySnapshot snapshot = await FirebaseFirestore.instance
           .collection('meals')
-          .where('dietId', isEqualTo: dietId)  // Query meals where 'dietId' matches
-          .where('category', isEqualTo: category)  // Query meals where 'category' matches
+          .where('dietId', isEqualTo: dietId)
+          .where('category', isEqualTo: category)
           .get();
 
       setState(() {
-        meals = snapshot.docs.map((doc) => Meal.fromFirestore(doc)).toList();  // Map each document to a Meal object and update the state
+        meals = snapshot.docs
+            .where((doc) => doc['category'] != null) // Filter out null 'category' values
+            .map((doc) => Meal.fromFirestore(doc))
+            .toList();
       });
 
-      print('Meals fetched: $meals');  // Print fetched meals
-
-    } catch (e) {
+      print('Meals fetched: $meals');
+    } catch (e, printStack) {
       print('Error fetching meals: $e');
+      print(printStack);
     }
   }
 
@@ -78,12 +78,12 @@ class _MealPlannerState extends State<MealPlanner> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Meal Planner"),  // Title of the AppBar
+        title: const Text("Diet Planner"),
       ),
       body: Column(
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.center,  // Center align the row
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
                 padding: const EdgeInsets.all(8),
@@ -92,90 +92,135 @@ class _MealPlannerState extends State<MealPlanner> {
                   gradient: LinearGradient(
                     colors: [TColor.secondaryColor1, TColor.secondaryColor2],
                     begin: Alignment.topLeft,
-                    end: Alignment.bottomRight
-                  )
+                    end: Alignment.bottomRight,
+                  ),
                 ),
                 child: DropdownButton<String>(
-                  value: selectedDietName,  // Currently selected diet name
-                  onChanged: (newValue) {  // Callback when a new diet type is selected
+                  value: selectedDietName,
+                  onChanged: (newValue) {
                     setState(() {
-                      selectedDietId = dietTypes.firstWhere(
-                              (diet) => diet['name'] == newValue)['dietId']!;  // Find the selected diet ID
-                      selectedDietName = newValue!;  // Update the selected diet name
+                      selectedDietId =
+                      dietTypes.firstWhere((diet) => diet['name'] == newValue)['dietId']!;
+                      selectedDietName = newValue!;
                     });
 
-                    print('Selected dietId: $selectedDietId, dietName: $selectedDietName');  // Print selected diet ID and name
+                    print('Selected dietId: $selectedDietId, dietName: $selectedDietName');
 
-                    _fetchMeals(selectedDietId, selectedCategory);  // Fetch meals for the selected diet and category
+                    _fetchMeals(selectedDietId, selectedCategory);
                   },
-                  items: dietTypes.map<DropdownMenuItem<String>>((diet) {  // Create dropdown items from diet types
-                    return DropdownMenuItem<String>(
+                  items: dietTypes
+                      .map<DropdownMenuItem<String>>(
+                        (diet) => DropdownMenuItem<String>(
                       value: diet['name'],
                       child: Text(diet['name']!),
-                    );
-                  }).toList(),
+                    ),
+                  )
+                      .toList(),
                 ),
               ),
-
-
               const SizedBox(width: 20),
-
               Container(
                 padding: const EdgeInsets.all(8),
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    gradient: LinearGradient(
-                        colors: [TColor.secondaryColor1, TColor.secondaryColor2],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight
-                    )
+                  borderRadius: BorderRadius.circular(12),
+                  gradient: LinearGradient(
+                    colors: [TColor.secondaryColor1, TColor.secondaryColor2],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
                 ),
                 child: DropdownButton<String>(
-                  value: selectedCategory,  // Currently selected category
-                  onChanged: (newValue) {  // Callback when a new category is selected
+                  value: selectedCategory,
+                  onChanged: (newValue) {
                     setState(() {
-                      selectedCategory = newValue!;  // Update the selected category
+                      selectedCategory = newValue!;
                     });
-                    print('Selected category: $selectedCategory');  // Print selected category
-                    _fetchMeals(selectedDietId, selectedCategory);  // Fetch meals for the selected diet and category
+                    print('Selected category: $selectedCategory');
+                    _fetchMeals(selectedDietId, selectedCategory);
                   },
-                  items: ['breakfast', 'lunch', 'dinner', 'snack'].map<DropdownMenuItem<String>>((String category) {  // Create dropdown items for categories
-                    return DropdownMenuItem<String>(
+                  items: ['breakfast', 'lunch', 'dinner', 'snack']
+                      .map<DropdownMenuItem<String>>(
+                        (category) => DropdownMenuItem<String>(
                       value: category,
                       child: Text(category),
-                    );
-                  }).toList(),
+                    ),
+                  )
+                      .toList(),
                 ),
               ),
             ],
           ),
-
           const SizedBox(height: 20),
-          // ListView to display the fetched meals
           Expanded(
             child: ListView.builder(
-              itemCount: meals.length,  // Number of meals to display
+              itemCount: meals.length,
               itemBuilder: (context, index) {
-                Meal meal = meals[index];  // Get the meal at the current index
-                return ListTile(
-                  leading: Image.network(
-                      meal.image,
-                      fit: BoxFit.cover),
-
-                  title: Text(meal.name),
-                  subtitle: Text(
-                    "Calories: ${meal.calories},"
-                        " Fat: ${meal.fat}, "
-                        "Recommended Plate: ${meal.recommend}",
+                Meal meal = meals[index];
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
                   ),
-                  onTap: (){
-                    Navigator.push(
+                  child: ListTile(
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        meal.image,
+                        fit: BoxFit.cover,
+                        width: 80,
+                        height: 80,
+                      ),
+                    ),
+                    title: Text(
+                      meal.name,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 4),
+                        Row(
+                          children: [
+                            const Text(
+                              'Calories: ',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text('${meal.calories}'),
+                          ],
+                        ),
+                        const SizedBox(height: 4),
+                        Row(
+                          children: [
+                            const Text(
+                              'Fat: ',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text('${meal.fat}'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context)=> MealDetailScreen(meal: meal)
-                        )
-                    );
-                  },
+                          builder: (context) => MealDetailScreen(meal: meal),
+                        ),
+                      );
+                    },
+                  ),
                 );
               },
             ),
